fa4aa2a03da063bdd99a415b3d5cc9655e92b36f
Ghislain Demaël <ghislain.demael@gmail.com>
2023-06-30 12:41:32 +0200

    C++ 20 compat test

--- main.cpp
+++ main.cpp
@@ -1,13 +1,35 @@
+#include <array>
 #include <iostream>
-#include <limits>
+#include <span>
+
+struct Foo
+{
+    int a{ };
+    int b{ };
+    int c{ };
+};
+
+consteval int sum(std::span<const int> a) // std::span and consteval
+{
+    int s{ 0 };
+    for (auto n : a)
+        s += n;
+    return s;
+}
+
+auto sum(auto x, auto y) -> decltype(x + y) // abbreviated function templates
+{
+    return x + y;
+}
 
 int main()
 {
-    std::cout << "Hello, world!";
+    constexpr std::array a{ 3, 2, 1 };
+    constexpr int s{ sum(a) };
+    std::cout << s << '\n';
 
-    std::cin.clear(); // reset any error flags
-    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // ignore any characters in the input buffer until we find a newline
-    std::cin.get(); // get one more char from the user (waits for user to press enter)
+    Foo f1{ .a = 1, .c = 3 }; // designated initializers
+    std::cout << sum(f1.a, f1.c) << '\n';
 
     return 0;
 }
