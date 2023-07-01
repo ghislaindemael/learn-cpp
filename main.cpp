517067019a3f678926aa18cfead02739efbe5ba5
Ghislain Demaël <ghislain.demael@gmail.com>
2023-07-01 19:20:58 +0200

    Unused

--- main.cpp
+++ main.cpp
@@ -1,35 +1,8 @@
-#include <array>
-#include <iostream>
-#include <span>
-
-struct Foo
-{
-    int a{ };
-    int b{ };
-    int c{ };
-};
-
-consteval int sum(std::span<const int> a) // std::span and consteval
-{
-    int s{ 0 };
-    for (auto n : a)
-        s += n;
-    return s;
-}
-
-auto sum(auto x, auto y) -> decltype(x + y) // abbreviated function templates
-{
-    return x + y;
-}
-
 int main()
 {
-    constexpr std::array a{ 3, 2, 1 };
-    constexpr int s{ sum(a) };
-    std::cout << s << '\n';
+    [[maybe_unused]] int x { 5 };
 
-    Foo f1{ .a = 1, .c = 3 }; // designated initializers
-    std::cout << sum(f1.a, f1.c) << '\n';
+    // since x is [[maybe_unused]], no warning generated
 
     return 0;
 }
