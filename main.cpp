096c61368c105f42ae8ee56d4f52f4450157f651
Ghislain Demaël <ghislain.demael@gmail.com>
2023-07-04 22:02:17 +0200

    Lesson 2.4 - Quiz Qs 4 & 5

--- main.cpp
+++ main.cpp
@@ -1,19 +1,21 @@
 #include <iostream>
 
+int doubleNumber(int num){
+    return num * 2;
+}
+
+int getNumberFromUser(){
+    std::cout << "Enter an integer: ";
+    int val { };
+    std::cin >> val;
+    return val;
+}
+
 int main()
 {
-
-    int val1 = { };
-    int val2 = { };
-
-    std::cout << "Enter an integer: ";
-    std::cin >> val1;
-    std::cout << "Enter another integer: ";
-    std::cin >> val2;
-
-
-    std::cout << val1 << " + " << val2 << " is " << val1 + val2 << ".\n";
-    std::cout << val1 << " - " << val2 << " is " << val1 - val2 << ".\n";
+    int choice { getNumberFromUser() };
+    int doubled { doubleNumber(choice) };
+    std::cout << "Double of " << choice << " is " << doubled <<".\n";
 
     return 0;
 }
