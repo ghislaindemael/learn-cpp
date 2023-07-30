03b02deffd6f689e0e023881bdc0d9a370687d99
Ghislain Demaël <ghislain.demael@gmail.com>
2023-07-30 17:37:05 +0200

    Lesson 4.5 - Playing around with types

--- main.cpp
+++ main.cpp
@@ -1,23 +1,24 @@
 #include <iostream>
 
-int readNumber()
-{
-    std::cout << "Please enter a number: ";
-    int x {};
-    std::cin >> x;
-    return x;
-}
-
-void writeAnswer(int x)
-{
-    std::cout << "The quotient is: " << x << '\n';
-}
-
 int main()
 {
-    int x{ readNumber() };
-    int y{ readNumber()};
-    writeAnswer(x/y);
+    unsigned short x{ 65535 }; // largest 16-bit unsigned value possible
+    std::cout << "x was: " << x << '\n';
+
+    x = 65536; // 65536 is out of our range, so we get modulo wrap-around
+    std::cout << "x is now: " << x << '\n';
+
+    x = 65537; // 65537 is out of our range, so we get modulo wrap-around
+    std::cout << "x is now: " << x << '\n';
+
+    unsigned short y{ 0 }; // smallest 2-byte unsigned value possible
+    std::cout << "y was: " << y << '\n';
+
+    y = -1; // -1 is out of our range, so we get modulo wrap-around
+    std::cout << "y is now: " << y << '\n';
+
+    y = -2; // -2 is out of our range, so we get modulo wrap-around
+    std::cout << "y is now: " << y << '\n';
 
     return 0;
 }
