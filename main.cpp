45db77145b3a76c6f834a46ebf733c0bad188c6a
Ghislain Demaël <ghislain.demael@gmail.com>
2023-07-30 14:51:45 +0200

    Chapter 3 - Quiz Q2

--- main.cpp
+++ main.cpp
@@ -2,22 +2,22 @@
 
 int readNumber()
 {
-    int x {};
     std::cout << "Please enter a number: ";
+    int x {};
     std::cin >> x;
     return x;
 }
 
 void writeAnswer(int x)
 {
-    std::cout << "The sum is: " << x << '\n';
+    std::cout << "The quotient is: " << x << '\n';
 }
 
 int main()
 {
-    int x { readNumber() };
-    x = x + readNumber();
-    writeAnswer(x);
+    int x{ readNumber() };
+    int y{ readNumber()};
+    writeAnswer(x/y);
 
     return 0;
 }
