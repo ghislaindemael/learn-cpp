0d56e340a85773a522e701798f842c27dee21902
Ghislain Demaël <ghislain.demael@gmail.com>
2023-08-02 14:15:22 +0200

    Lesson O.4 - Quiz Q6

--- main.cpp
+++ main.cpp
@@ -1,26 +1,35 @@
 #include <bitset>
-#include <cstdint>
 #include <iostream>
+
+int askForInt255(){
+    std::cout << "Enter an integer between 0 and 255: ";
+    int i {};
+    std::cin >> i;
+    return i;
+}
 
 int main()
 {
-    [[maybe_unused]] constexpr std::uint8_t option_viewed{ 0x01 };
-    [[maybe_unused]] constexpr std::uint8_t option_edited{ 0x02 };
-    [[maybe_unused]] constexpr std::uint8_t option_favorited{ 0x04 };
-    [[maybe_unused]] constexpr std::uint8_t option_shared{ 0x08 };
-    [[maybe_unused]] constexpr std::uint8_t option_deleted{ 0x10 };
+    int i { askForInt255() };
 
-    std::uint8_t myArticleFlags{ option_favorited };
+    int b0 { i / 128};
+    i %= 128;
+    int b1 { i / 64};
+    i %= 64;
+    int b2 { i / 32};
+    i %= 32;
+    int b3 { i / 16};
+    i %= 16;
+    int b4 { i / 8};
+    i %= 8;
+    int b5 { i / 4};
+    i %= 4;
+    int b6 { i / 2};
+    i %= 2;
+    int b7 { i / 1};
+    i %= 1;
 
-    myArticleFlags |= option_viewed;
-
-    if(myArticleFlags & option_deleted){
-        std::cout << "The item was deleted.";
-    }
-
-    myArticleFlags &= ~option_favorited;
-
-    std::cout << std::bitset<8>{ myArticleFlags } << '\n';
+    std::cout << b0 << b1 << b2 << b3 << b4 << b5 << b6 << b7;
 
     return 0;
 }
