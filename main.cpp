2c5ab41d69a4e591ab8e766f2835345b4b41e314
Ghislain Demaël <ghislain.demael@gmail.com>
2023-08-02 13:59:22 +0200

    Lesson O.3 - Quiz Q1

--- main.cpp
+++ main.cpp
@@ -1,22 +1,26 @@
 #include <bitset>
+#include <cstdint>
 #include <iostream>
-
-// "rotl" stands for "rotate left"
-std::bitset<4> rotl(std::bitset<4> bits)
-{
-    std::bitset<4> firstBit = bits >> 3;
-    bits <<=1;
-    bits |= firstBit;
-    return bits;
-}
 
 int main()
 {
-    std::bitset<4> bits1{ 0b0001 };
-    std::cout << rotl(bits1) << '\n';
+    [[maybe_unused]] constexpr std::uint8_t option_viewed{ 0x01 };
+    [[maybe_unused]] constexpr std::uint8_t option_edited{ 0x02 };
+    [[maybe_unused]] constexpr std::uint8_t option_favorited{ 0x04 };
+    [[maybe_unused]] constexpr std::uint8_t option_shared{ 0x08 };
+    [[maybe_unused]] constexpr std::uint8_t option_deleted{ 0x10 };
 
-    std::bitset<4> bits2{ 0b1001 };
-    std::cout << rotl(bits2) << '\n';
+    std::uint8_t myArticleFlags{ option_favorited };
+
+    myArticleFlags |= option_viewed;
+
+    if(myArticleFlags & option_deleted){
+        std::cout << "The item was deleted.";
+    }
+
+    myArticleFlags &= ~option_favorited;
+
+    std::cout << std::bitset<8>{ myArticleFlags } << '\n';
 
     return 0;
 }
