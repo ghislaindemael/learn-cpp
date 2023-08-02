5e0755ed1d7318d81af68a3a7be3eeec09cf7423
Ghislain Demaël <ghislain.demael@gmail.com>
2023-08-02 11:43:47 +0200

    Lesson 5.3 - Quiz Q2

--- main.cpp
+++ main.cpp
@@ -1,31 +1,24 @@
 #include <iostream>
 
-int askForHeight(){
-    std::cout << "Enter the height of the tower in meters: ";
-    int h {};
-    std::cin >> h;
-    return h;
+int askForInt(){
+    std::cout << "Enter an integer: ";
+    int i {};
+    std::cin >> i;
+    return i;
+}
+
+int isEven(int x){
+    return (x % 2 == 0);
 }
 
 int main()
 {
-    constexpr double gravity = 9.8;
-    int tower = askForHeight();
-    int seconds { 0 };
-    bool exit { false };
-    double height;
-
-    while (!exit) {
-        height = tower - ((gravity * seconds * seconds) / 2);
-        if (height < 0) {
-            std::cout << "At " << seconds << " seconds, the ball is on the ground.\n";
-            exit = true;
-        } else {
-            std::cout << "At " << seconds << " seconds, the ball is at height: " << height << " meters.\n";
-            seconds = seconds + 1;
-        }
+    int i { askForInt() };
+    if (isEven(i)){
+        std::cout << i << " is even.";
+    } else {
+        std::cout << i << " is odd.";
     }
-
 
     return 0;
 }
