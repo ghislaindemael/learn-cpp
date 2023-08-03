061d6a97d41cf2a1616878adae95951463d7e0fe
Ghislain Demaël <ghislain.demael@gmail.com>
2023-08-03 15:29:23 +0200

    Lesson 7.10 - Quiz Q4

--- main.cpp
+++ main.cpp
@@ -7,18 +7,25 @@ int askForInt(){
     return x;
 }
 
-int sumTo(int x){
-    int sum { 0 };
-    for( int i { x }; i >=0 ; --i){
-        sum += i;
+void fizzbuzz(int x){
+    for(int i { 1 }; i <= x; ++i){
+
+        if(i % 3 == 0 && i % 5 == 0){
+            std::cout << "fizzbuzz" << "\n";
+        } else if (i % 3 == 0) {
+            std::cout << "fizz" << "\n";
+        } else if (i % 5 == 0){
+            std::cout << "buzz" << "\n";
+        } else {
+            std::cout << i << "\n";
+        }
     }
-    return sum;
 }
 
 int main()
 {
 
-    std::cout << sumTo(askForInt());
+    fizzbuzz(askForInt());
 
     return 0;
 }
