69fb755b4edbe68f9906af4fa0586f543f734b1a
Ghislain Demaël <ghislain.demael@gmail.com>
2023-08-03 14:50:57 +0200

    Lesson 7.8 - Quiz Q2

--- main.cpp
+++ main.cpp
@@ -1,44 +1,13 @@
 #include <iostream>
 
-int askForInt(){
-    std::cout << "Enter an integer: ";
-    int i {};
-    std::cin >> i;
-    return i;
-}
-
-char askForOperator(){
-    std::cout << "Enter one of the following (+, -, *, /, %): ";
-    char o {};
-    std::cin >> o;
-    return o;
-}
-
 int main()
 {
-    int i1 { askForInt() };
-    int i2 { askForInt() };
-    int result {};
+    char c { 97 };
 
-    switch (askForOperator()) {
-        case '+':
-            result = i1 + i2;
-            break;
-        case '-':
-            result = i1 - i2;
-            break;
-        case '*':
-            result = i1 * i2;
-            break;
-        case '/':
-            result = i1 / i2;
-            break;
-        case '%':
-            result = i1 % i2;
-            break;
+    while (c <= 122){
+        std::cout << c << ' ' << static_cast<int>(c) << "\n";
+        ++c;
     }
-
-    std::cout << "The result is " << result << "\n";
 
 
     return 0;
