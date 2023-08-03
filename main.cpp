c07f62152a25db45d30a0a1082616f9d4f955b89
Ghislain Demaël <ghislain.demael@gmail.com>
2023-08-03 10:54:04 +0200

    Chapter 6 - Quiz Q3

--- main.cpp
+++ main.cpp
@@ -1,17 +1,18 @@
 #include <iostream>
-#include "constants.h"
+
+int accumulate(int x){
+
+    static int s_sum {0};
+    s_sum += x;
+    return s_sum;
+}
 
 int main()
 {
-    std::cout << "How many students are in your class? ";
-    int students{};
-    std::cin >> students;
-
-
-    if (students > constants::max_class_size)
-        std::cout << "There are too many students in this class";
-    else
-        std::cout << "This class isn't too large";
+    std::cout << accumulate(4) << '\n'; // prints 4
+    std::cout << accumulate(3) << '\n'; // prints 7
+    std::cout << accumulate(2) << '\n'; // prints 9
+    std::cout << accumulate(1) << '\n'; // prints 10
 
     return 0;
 }
