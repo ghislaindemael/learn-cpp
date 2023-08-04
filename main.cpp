100df6b18fa84af9f28eab75d9c84389b4134121
Ghislain Demaël <ghislain.demael@gmail.com>
2023-08-04 22:41:03 +0200

    Chapter 8 - Quiz Q2

--- main.cpp
+++ main.cpp
@@ -1,7 +1,26 @@
-using PrintError = int;
-PrintError printData();
+#include <iostream>
 
-int main(){
+namespace constants
+{
+    inline constexpr double pi{ 3.14159 };
+}
+
+using Degrees = double;
+using Radians = double;
+
+Radians convertToRadians(Degrees degrees)
+{
+    return degrees * constants::pi / 180;
+}
+
+int main()
+{
+    std::cout << "Enter a number of degrees: ";
+    Degrees degrees{};
+    std::cin >> degrees;
+
+    Radians radians{ convertToRadians(degrees) };
+    std::cout << degrees << " degrees is " << radians << " radians.\n";
 
     return 0;
-};
+}
