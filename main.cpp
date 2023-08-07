5d3e0756078a82301e85d038bb9d7a67062c32b4
Ghislain Demaël <ghislain.demael@gmail.com>
2023-08-07 14:24:13 +0200

    Lesson 10.4 - Quiz Q1

--- main.cpp
+++ main.cpp
@@ -1,20 +1,52 @@
 #include <iostream>
 
-namespace Monster {
-    enum MonsterType {
-        orc,
-        goblin,
-        troll,
-        ogre,
-        skeleton,
-    };
+enum class Animal {
+    pig,
+    chicken,
+    goat,
+    cat,
+    dog,
+    duck,
+};
+
+std::string_view getAnimalName(Animal a){
+    using enum Animal;
+    switch (a) {
+        case pig: return "pig";
+        case chicken: return "chicken";
+        case goat: return "goat";
+        case cat: return "cat";
+        case dog: return "dog";
+        case duck: return "duck";
+        default: return "???";
+    }
 }
+
+int getNumberOfLegs(Animal a){
+    using enum Animal;
+    switch (a) {
+        case pig:
+        case goat:
+        case cat:
+        case dog:
+            return 4;
+        case chicken:;
+        case duck:
+            return 2;
+        default:
+            return 0;
+    }
+}
+
+void printNumberOfLegs(Animal a){
+    std::cout << "A " << getAnimalName(a) << " has " << getNumberOfLegs(a) << " legs.\n";
+}
+
 
 int main()
 {
-
-    Monster::MonsterType troll {Monster::troll };
-    std::cout << troll;
+    printNumberOfLegs(Animal::cat);
+    printNumberOfLegs(Animal::chicken);
 
     return 0;
 }
