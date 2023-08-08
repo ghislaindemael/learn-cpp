df488ee23fa9dde86124a04ec8b8e0720dd2ceb7
Ghislain Demaël <ghislain.demael@gmail.com>
2023-08-08 17:00:57 +0200

    Lesson 11.11 - Quiz Q1

--- main.cpp
+++ main.cpp
@@ -1,29 +1,46 @@
+#include <algorithm>
 #include <iostream>
-#include <iterator>
+#include <string>
 
-int* findValue(int* ptr1, int* ptr2, int num){
-    int* ptr { ptr1 };
-    while(ptr != ptr2){
-        if(*ptr == num){
-            return ptr;
-        }
-        ++ptr;
+std::size_t getNameCount()
+{
+    std::cout << "How many names would you like to enter? ";
+    std::size_t length{};
+    std::cin >> length;
+
+    return length;
+}
+
+// Asks user to enter all the names
+void getNames(std::string* names, std::size_t length)
+{
+    for (std::size_t i{ 0 }; i < length; ++i)
+    {
+        std::cout << "Enter name #" << i + 1 << ": ";
+        std::getline(std::cin >> std::ws, names[i]);
     }
-    return ptr2;
+}
+
+// Prints the sorted names
+void printNames(std::string* names, std::size_t length)
+{
+    std::cout << "\nHere is your sorted list:\n";
+
+    for (std::size_t i{ 0 }; i < length; ++i)
+        std::cout << "Name #" << i + 1 << ": " << names[i] << '\n';
 }
 
 int main()
 {
-    int arr[]{ 2, 5, 4, 10, 8, 22, 16, 40 };
+    std::size_t length{ getNameCount() };
 
-    // Search for the first element with value 20.
-    int* found{ findValue(std::begin(arr), std::end(arr), 20) };
+    auto* names{ new std::string[length]{} };
 
-    // If an element with value 20 was found, print it.
-    if (found != std::end(arr))
-    {
-        std::cout << *found << '\n';
-    }
+    getNames(names, length);
+    std::sort(names, names + length);
+
+    printNames(names, length);
+    delete[] names;
 
     return 0;
 }
