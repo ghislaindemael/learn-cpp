87347cd3b00dff6073a930876c1462ce40176a29
Ghislain Demaël <ghislain.demael@gmail.com>
2023-08-08 16:31:27 +0200

    Lesson 11.4 - Quiz Q4

--- main.cpp
+++ main.cpp
@@ -7,11 +7,17 @@ int main()
     int array[]{ 6, 3, 2, 9, 7, 1, 5, 4, 8 };
     constexpr int length{ static_cast<int>(std::size(array)) };
 
-    for (int startIndex{ 0 }; startIndex < length; ++startIndex) {
-        for(int nestedIndex { 0 }; nestedIndex < length - 1; ++nestedIndex){
+    for (int startIndex{ 0 }; startIndex < length - 1; ++startIndex) {
+        bool sorted { true } ;
+        for(int nestedIndex { 0 }; nestedIndex < length - startIndex - 1; ++nestedIndex){
             if(array[nestedIndex] > array[nestedIndex + 1]){
+                sorted = false;
                 std::swap(array[nestedIndex], array[nestedIndex + 1]);
             }
+        }
+        if(sorted){
+            std::cout << "Early termination on iteration " << startIndex + 1 << "\n";
+            break;
         }
     }
 
