d01ba6af6c5c1ceabf66392d71d21f43ec931b9d
Ghislain Demaël <ghislain.demael@gmail.com>
2023-08-08 10:13:10 +0200

    Lesson 11.3 - Quiz Q2

--- main.cpp
+++ main.cpp
@@ -5,9 +5,30 @@ int main()
 {
     constexpr int array[]{ 4, 6, 7, 3, 8, 2, 1, 9, 5 };
 
-    for (int i = 0; i < static_cast<int>(std::size(array)); ++i) {
-        std::cout << array[i] << "\n";
+    int toSearch {};
+    do {
+        std::cout << "Enter a number between 1 and 9: ";
+        std::cin >> toSearch;
+
+        if (std::cin.fail())
+        {
+            std::cin.clear(); // reset any error flags
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // ignore any characters in the input buffer
+        }
+    } while (toSearch < 1 || toSearch > 9);
+
+    for(int i {0}; i < 9; ++i){
+        std::cout << array[i] << ' ';
     }
+    std::cout << "\n";
+
+    for(int i {0}; i < 9; ++i){
+        if(array[i] == toSearch){
+            std::cout << "The number " << toSearch << " has index " << i << "\n";
+        }
+    }
+
+
 
     return 0;
 }
