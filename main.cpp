22aaceb72f308758bdceb2d0affc65aef7f78ffa
Ghislain Demaël <ghislain.demael@gmail.com>
2023-08-08 16:26:38 +0200

    Lesson 11.4 - Quiz Q3

--- main.cpp
+++ main.cpp
@@ -4,35 +4,21 @@
 
 int main()
 {
-    int array[]{ 30, 50, 20, 10, 40 };
+    int array[]{ 6, 3, 2, 9, 7, 1, 5, 4, 8 };
     constexpr int length{ static_cast<int>(std::size(array)) };
 
-    // Step through each element of the array
-    // (except the last one, which will already be sorted by the time we get there)
-    for (int startIndex{ 0 }; startIndex < length - 1; ++startIndex)
-    {
-        // smallestIndex is the index of the smallest element we’ve encountered this iteration
-        // Start by assuming the smallest element is the first element of this iteration
-        int highestIndex{ startIndex };
-
-        // Then look for a smaller element in the rest of the array
-        for (int currentIndex{ startIndex + 1 }; currentIndex < length; ++currentIndex)
-        {
-            // If we've found an element that is smaller than our previously found smallest
-            if (array[currentIndex] > array[highestIndex])
-                // then keep track of it
-                highestIndex = currentIndex;
+    for (int startIndex{ 0 }; startIndex < length; ++startIndex) {
+        for(int nestedIndex { 0 }; nestedIndex < length - 1; ++nestedIndex){
+            if(array[nestedIndex] > array[nestedIndex + 1]){
+                std::swap(array[nestedIndex], array[nestedIndex + 1]);
+            }
         }
-
-        // smallestIndex is now the index of the smallest element in the remaining array
-        // swap our start element with our smallest element (this sorts it into the correct place)
-        std::swap(array[startIndex], array[highestIndex]);
     }
 
     // Now that the whole array is sorted, print our sorted array as proof it works
-    for (int index{ 0 }; index < length; ++index)
+    for (int index{ 0 }; index < length; ++index) {
         std::cout << array[index] << ' ';
-
+    }
     std::cout << '\n';
 
     return 0;
