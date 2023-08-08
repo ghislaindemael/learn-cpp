b10e4af68c0b20f210caa21abd6e1b4f9b2c2f2c
Ghislain Demaël <ghislain.demael@gmail.com>
2023-08-09 00:27:32 +0200

    Lesson 11.12 - Quiz Q1

--- main.cpp
+++ main.cpp
@@ -1,46 +1,24 @@
-#include <algorithm>
 #include <iostream>
 #include <string>
 
-std::size_t getNameCount()
-{
-    std::cout << "How many names would you like to enter? ";
-    std::size_t length{};
-    std::cin >> length;
-
-    return length;
-}
-
-// Asks user to enter all the names
-void getNames(std::string* names, std::size_t length)
-{
-    for (std::size_t i{ 0 }; i < length; ++i)
-    {
-        std::cout << "Enter name #" << i + 1 << ": ";
-        std::getline(std::cin >> std::ws, names[i]);
-    }
-}
-
-// Prints the sorted names
-void printNames(std::string* names, std::size_t length)
-{
-    std::cout << "\nHere is your sorted list:\n";
-
-    for (std::size_t i{ 0 }; i < length; ++i)
-        std::cout << "Name #" << i + 1 << ": " << names[i] << '\n';
-}
-
 int main()
 {
-    std::size_t length{ getNameCount() };
+    std::string_view names[]{ "Alex", "Betty", "Caroline", "Dave", "Emily", "Fred", "Greg", "Holly"};
+    std::cout << "Enter a name: ";
+    std::string input { };
+    std::cin >> input;
 
-    auto* names{ new std::string[length]{} };
+    bool found { false };
+    for(std::string_view sv : names){
+        if(sv == input){
+            std::cout << sv << " was found !";
+            found = true;
+        }
+    }
 
-    getNames(names, length);
-    std::sort(names, names + length);
-
-    printNames(names, length);
-    delete[] names;
+    if(!found){
+        std::cout << input << " was not found.";
+    }
 
     return 0;
 }
