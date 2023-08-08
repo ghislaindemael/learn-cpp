20ee9ef4b12e15206559a6f3ea4d69c80a1bc978
Ghislain Demaël <ghislain.demael@gmail.com>
2023-08-08 09:52:42 +0200

    Lesson 11.2 - Quiz Q2

--- main.cpp
+++ main.cpp
@@ -1,15 +1,24 @@
 #include <iostream>
 #include <string>
 
+namespace {
+    enum Animal{
+        chicken,
+        dog,
+        cat,
+        elephant,
+        duck,
+        snake,
+
+        numOfAnimals
+    };
+}
+
 int main()
 {
-    double highestTemp[365]{};
-    for (double & i : highestTemp) {
-        i = 0.0;
-    }
-    std::cout << highestTemp[54];
+    int numOfLegs[numOfAnimals]{2, 4, 4, 4, 2, 0};
 
-
+    std::cout << "An elephant has " << numOfLegs[elephant] << " legs.";
 
     return 0;
 }
