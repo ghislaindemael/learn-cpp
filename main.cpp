a85b4711cdbe73b062a2a7dbc0303b0a1b7240f8
Ghislain Demaël <ghislain.demael@gmail.com>
2023-08-09 09:42:17 +0200

    Chapter 11 - Quiz Q2

--- main.cpp
+++ main.cpp
@@ -1,31 +1,69 @@
-#include <array>
-#include <numeric> // std::reduce
+#include <algorithm> // std::sort
+#include <cstddef> // std::size_t
 #include <iostream>
+#include <string>
+#include <vector>
 
-// We want to use ItemTypes to index an array. Use enum rather than enum class.
-enum ItemTypes
+struct Student
 {
-    item_health_potion,
-    item_torch,
-    item_arrow,
-    max_items
+    std::string name{};
+    int grade{};
 };
 
-using Inventory = std::array<int, ItemTypes::max_items>;
-
-int countTotalItems(const Inventory& items)
+int getNumberOfStudents()
 {
-    return std::reduce(items.begin(), items.end());
+    int numberOfStudents{};
+
+    do
+    {
+        std::cout << "How many students do you want to enter? ";
+        std::cin >> numberOfStudents;
+    } while (numberOfStudents <= 0);
+
+    return numberOfStudents;
+}
+
+std::vector<Student> getStudents()
+{
+    using Students = std::vector<Student>;
+
+    int numberOfStudents{ getNumberOfStudents() };
+
+    // Create a vector with numberOfStudents elements.
+    Students students(static_cast<Students::size_type>(numberOfStudents));
+
+    int studentNumber{ 1 };
+
+    for (auto& student : students)
+    {
+        std::cout << "Enter name #" << studentNumber << ": ";
+        std::cin >> student.name;
+        std::cout << "Enter grade #" << studentNumber << ": ";
+        std::cin >> student.grade;
+
+        ++studentNumber;
+    }
+
+    return students;
+}
+
+// Pass by reference to avoid slow copies.
+bool compareStudents(const Student& a, const Student& b)
+{
+    return (a.grade > b.grade);
 }
 
 int main()
 {
-    Inventory items{ 2, 5, 10 };
+    auto students{ getStudents() };
 
-    std::cout << "The player has " << countTotalItems(items) << " item(s) in total.\n";
+    std::sort(students.begin(), students.end(), compareStudents);
 
-    // We can access individual items using the enumerators:
-    std::cout << "The player has " << items[ItemTypes::item_torch] << " torch(es)\n";
+    // Print out all the names
+    for (const auto& student : students)
+    {
+        std::cout << student.name << " got a grade of " << student.grade << '\n';
+    }
 
     return 0;
 }
