36c96a3312a5058a6cfe890e2bc06de62e5de893
Ghislain Demaël <ghislain.demael@gmail.com>
2023-08-09 11:51:25 +0200

    Lesson 124 - Quiz Q1

--- main.cpp
+++ main.cpp
@@ -1,59 +1,17 @@
 #include <iostream>
 #include <functional>
 
-using ArithmeticFunction = std::function<int(int, int)>;
-
-int askForInt(){
-    std::cout << "Enter an integer: ";
-    int x { };
-    std::cin >> x;
-    return x;
-}
-
-char askForOper(){
-    char o{};
-    do {
-        std::cout << "Enter one of the following (+, -, *, /): ";
-        std::cin >> o;
-    } while (o != '+' && o != '-' && o != '*' && o != '/');
-    return o;
-}
-
-int add(int x, int y){
-    return x + y;
-}
-
-int subtract(int x, int y){
-    return x - y;
-}
-
-int mult(int x, int y){
-    return x * y;
-}
-
-int divide(int x, int y){
-    return x / y;
-}
-
-ArithmeticFunction getArithmeticFunction(char op){
-    switch (op) {
-        case '+': return &add;
-        case '-': return &subtract;
-        case '*': return &mult;
-        case '/': return &divide;
+int factorial(int n){
+    if(n == 0 || n == 1){
+        return 1;
+    } else {
+        return n * factorial(n - 1);
     }
-    return nullptr;
 }
 
 int main()
 {
-    int a { askForInt() };
-    char op { askForOper() };
-    int b { askForInt() };
-
-    ArithmeticFunction fcn {getArithmeticFunction(op)};
-
-    std::cout << a << ' ' << op << ' ' << b << " = " << fcn(a,b);
+    std::cout << "Factorial of 7 is: " << factorial(7) << "\n";
 
     return 0;
 }
