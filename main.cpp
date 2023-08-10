11e2ebc7eeebd22a296a5a9d5a6c7965dac2d725
Ghislain Demaël <ghislain.demael@gmail.com>
2023-08-10 09:25:16 +0200

    Lesson 13.2 - Quiz Q1

--- main.cpp
+++ main.cpp
@@ -1,20 +1,29 @@
 #include <iostream>
 
-void printBinary(int x)
-{
-	if (x == 0) {
-        return;
+class IntPair {
+public:
+    int i1 {};
+    int i2 {};
+
+    void print(){
+        std::cout << "Pair(" << i1 << ", " << i2 <<")\n";
     }
 
-	printBinary(x / 2);
-	std::cout << x % 2;
-}
+    void set(int i, int j){
+        i1 = i;
+        i2 = j;
+    }
+};
 
 int main()
 {
-	int x;
-	std::cout << "Enter a positive integer: ";
-	std::cin >> x;
+    IntPair p1;
+    p1.set(1, 1); // set p1 values to (1, 1)
 
-	printBinary(x);
+    IntPair p2 { 2, 2 }; // initialize p2 values to (2, 2)
+
+    p1.print();
+    p2.print();
+
+    return 0;
 }
