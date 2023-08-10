aac4e853ed507761cfedce8200fcaae3f893a4c8
Ghislain Demaël <ghislain.demael@gmail.com>
2023-08-10 17:59:08 +0200

    Lesson 13.6 - Quiz Q1

--- main.cpp
+++ main.cpp
@@ -1,39 +1,35 @@
 #include <iostream>
 #include <cassert>
+#include <cstdint>
 
-class Ball {
-    std::string_view m_color { "black" };
-    double m_radius { 10.0 };
+class RGBA {
+    std::uint8_t m_red;
+    std::uint8_t m_green;
+    std::uint8_t m_blue;
+    std::uint8_t m_alpha;
 public:
-    Ball() = default;
-    Ball(double d){
-        m_radius = d;
-    }
-    Ball(std::string_view s){
-        m_color = s;
-    }
-    Ball(std::string_view s, double d){
-        m_color = s;
-        m_radius = d;
-    }
+
+    RGBA(std::uint8_t i_r = 0, std::uint8_t i_g = 0, std::uint8_t i_b = 0, std::uint8_t i_a = 255)
+    : m_red { i_r }
+    , m_green { i_g }
+    , m_blue { i_b }
+    , m_alpha { i_a }
+    {}
+
     void print(){
-        std::cout << "color: " << m_color << ", radius: " << m_radius << "\n";
+        std::cout
+        << "r=" << static_cast<int>(m_red)
+        << " g=" << static_cast<int>(m_green)
+        << " b=" << static_cast<int>(m_blue)
+        << " a=" << static_cast<int>(m_alpha)
+        << "\n";
     }
 };
 
 int main()
 {
-    Ball def{};
-    def.print();
-
-    Ball blue{ "blue" };
-    blue.print();
-
-    Ball twenty{ 20.0 };
-    twenty.print();
-
-    Ball blueTwenty{ "blue", 20.0 };
-    blueTwenty.print();
+    RGBA teal{ 0, 127, 127 };
+    teal.print();
 
     return 0;
 }
